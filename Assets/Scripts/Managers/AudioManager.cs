f359acbb14c69c9e9773ecf639ade47ba318c86c
DaLexusS <jenya.flieshman@gmail.com>
2026-02-07 22:35:47 +0200

    audio manager

---
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -0,0 +1,63 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Audio;
+
+
+//For each sound make a name inside this Enum!!!!!!
+public enum SFX
+{
+    Test,
+}
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance { get; private set; }
+
+    [Header("Settings")]
+    [SerializeField] public AudioMixer audioMixer; //Unused yet
+    [SerializeField] public AudioPool sfxPool;
+
+
+
+    //Add any sound you need here!!!!!!
+    [Header("Sounds")]
+    [Header("SFX")]
+    [SerializeField] public AudioClip anySound;
+    
+
+    public void Awake()
+    {
+        if (!Instance.IsUnityNull())
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void PlaySfx(float volume, AudioClip audio, float pitch)
+    {
+        sfxPool.PlaySound(volume, audio, pitch);
+    }
+
+    public void PlaySfx(float volume, SFX sfx, float pitch)
+    {
+
+        //Here you make switch for each sfx you have added and it plays it that's it!
+        switch (sfx)
+        {
+            case SFX.Test:
+                PlaySfx(volume, anySound, pitch);
+                break;
+            default:
+                break;
+        }
+    }
+
+    public float GetRandomPitch(float min, float max)
+    {
+        return Random.Range(min, max);
+    }
+}
